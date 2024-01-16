b60069dc1e3ad7ee0dd2914f4a98ed9a0cae0064
hajen <katja.lindeberg@prototyp.se>
2024-01-16 10:59:59 +0100

    refactor: event.js to use a class and separate fetch function

--- public/js/event.js
+++ public/js/event.js
@@ -1,29 +1,51 @@
-var urlParams = new URLSearchParams(window.location.search);
-var eventId = urlParams.get("id");
+class Event {
+  constructor(name, description, image, date, time, location) {
+    this.name = name;
+    this.description = description;
+    this.image = image;
+    this.date = date;
+    this.time = time;
+    this.location = location;
+  }
+}
 
-// Fetch the event data from the server
-fetch("/event/" + eventId)
-  .then((response) => {
-    console.log("Inside first then");
-    console.log(response);
-    if (!response.ok) {
-      console.error("Error status:", response.status);
-      console.error("Status text:", response.statusText);
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  })
-  .then((eventData) => {
-    console.log("Inside second then");
-    console.log(eventData);
-    // Populate the page with the event data
-    document.getElementById("eventName").textContent = eventData.name;
-    document.getElementById("eventDescription").textContent =
-      eventData.description;
-    console.log(eventData.description);
-  })
-  .catch((error) => {
-    console.log("Inside catch");
-    console.error("There has been a problem with your fetch operation:", error);
-  });
-// });
+const urlParams = new URLSearchParams(window.location.search);
+const eventId = urlParams.get("id");
+
+getEvent(eventId);
+
+function getEvent(eventId) {
+  fetch(`/event/${eventId}`)
+    .then((response) => {
+      if (!response.ok) {
+        console.error(`Error status: ${response.status}`);
+        console.error(`Status text: ${response.statusText}`);
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((eventData) => {
+      const event = new Event(
+        eventData.name,
+        eventData.description,
+        eventData.image,
+        eventData.date,
+        eventData.time,
+        eventData.location,
+      );
+
+      document.getElementById("eventName").textContent = event.name;
+      document.getElementById("eventDescription").textContent =
+        event.description;
+      document.getElementById("eventImage").src = event.image;
+      document.getElementById("eventDate").textContent = event.date;
+      document.getElementById("eventTime").textContent = event.time;
+      document.getElementById("eventLocation").textContent = event.location;
+    })
+    .catch((error) => {
+      console.error(
+        "There has been a problem with the fetch operation:",
+        error,
+      );
+    });
+}
