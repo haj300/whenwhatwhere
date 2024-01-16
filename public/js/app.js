cea6eed04fa977739f66270c3eb8ac8cf3778323
hajen <katja.lindeberg@prototyp.se>
2024-01-16 10:54:44 +0100

    style: update look of event

--- public/js/app.js
+++ public/js/app.js
@@ -10,11 +10,15 @@ async function getEvents() {
     const events = await response.json();
 
     events.forEach((event) => {
+      const eventTitle = document.createElement("h2");
       const eventItem = document.createElement("div");
-      eventItem.textContent = `${event.name}: ${event.eventStart.slice(
+      eventItem.classList.add("event-item");
+      eventTitle.textContent = event.name;
+      eventItem.textContent = `${event.description}: ${event.eventStart.slice(
         0,
         10,
       )} - ${event.eventStop.slice(0, 10)}`;
+      document.getElementById("eventList").appendChild(eventTitle);
       document.getElementById("eventList").appendChild(eventItem);
     });
   } catch (e) {
