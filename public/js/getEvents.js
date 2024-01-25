b926e1a48f54e85208dbd1e087c1139eaf2f11fc
hajen <katja.lindeberg@prototyp.se>
2024-01-25 14:10:29 +0100

    refactor: event rendering in getEvents.js

--- public/js/getEvents.js
+++ public/js/getEvents.js
@@ -4,29 +4,75 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function getEvents() {
   try {
-    document.getElementById("eventList").innerHTML = "";
+    const eventList = document.getElementById("eventList");
+    eventList.innerHTML = "";
 
     const response = await fetch("http://localhost:3000/events");
     const events = await response.json();
 
     events.forEach((event) => {
-      const eventTitle = document.createElement("h2");
-      const eventItem = document.createElement("div");
-      const eventImage = document.createElement("img");
-      eventItem.classList.add("event-item");
-      eventTitle.textContent = event.name;
-      eventItem.textContent = `${event.description}: ${event.date.slice(
-        0,
-        10,
-      )} at ${event.date.slice(11, 16)} ${event.location}`;
-      eventImage.src = event.image;
-      document.getElementById("eventList").appendChild(eventTitle);
-      document.getElementById("eventList").appendChild(eventItem);
-      document.getElementById("eventList").appendChild(eventImage);
+      const eventTitle = createEventTitle(event.name);
+      const eventItem = createEventItem(
+        event.description,
+        event.date,
+        event.location,
+      );
+      const eventImage = createEventImage(event.image);
+      const eventBackground = createEventBackground("orange");
+
+      appendEventElements(
+        eventList,
+        eventTitle,
+        eventItem,
+        eventImage,
+        eventBackground,
+      );
     });
   } catch (e) {
     console.error(e);
   }
+}
+
+function createEventTitle(name) {
+  const eventTitle = document.createElement("h2");
+  eventTitle.textContent = name;
+  return eventTitle;
+}
+
+function createEventItem(description, date, location) {
+  const eventItem = document.createElement("div");
+  eventItem.classList.add("event-item");
+  eventItem.textContent = `${description}: ${date.slice(0, 10)} at ${date.slice(
+    11,
+    16,
+  )} ${location}`;
+  return eventItem;
+}
+
+function createEventBackground(background) {
+  const eventBackground = document.createElement("div");
+  eventBackground.style.backgroundColor = background;
+  eventBackground.classList.add("event-item-background");
+  return eventBackground;
+}
+
+function createEventImage(image) {
+  const eventImage = document.createElement("img");
+  eventImage.src = image;
+  return eventImage;
+}
+
+function appendEventElements(
+  eventList,
+  eventTitle,
+  eventItem,
+  eventImage,
+  eventBackground,
+) {
+  eventBackground.appendChild(eventTitle);
+  eventBackground.appendChild(eventItem);
+  eventBackground.appendChild(eventImage);
+  eventList.appendChild(eventBackground);
 }
 
 export { getEvents };
